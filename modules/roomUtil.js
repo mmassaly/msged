6b9122c04c7f2728eeae42b0c72ecf6d40824553
MassalyJR <93945613+MassalyJR@users.noreply.github.com>
2025-08-04 14:43:23 +0000

    GO

--- modules/roomUtil.js
+++ modules/roomUtil.js
@@ -27,9 +27,9 @@ function roomUpdates(req,room,command)
 
     req.app.locals.sessions.forEach(session => 
     {
-        console.log("session room "+session.room.replace("//", path.sep));
-        console.log("session room "+session.room);
-         console.log("session room "+session.room.replace('\\',path.sep).session.room.replace("//", path.sep));
+        console.log("session room 1"+session.room.replace("//", path.sep));
+        console.log("session room 2"+session.room);
+         console.log("session room 3"+session.room.replace('\\',path.sep).session.room.replace("//", path.sep));
         if(command.isAdditionalCommand)
         {
             console.log("session room "+session.room);
