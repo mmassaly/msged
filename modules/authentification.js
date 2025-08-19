e49d74a3bcae5f1706b32e4a8a8371fa30cb5837
mmassaly <massalymamadou@gmail.com>
2025-08-19 21:16:25 +0000

    Fo

--- modules/authentification.js
+++ modules/authentification.js
@@ -73,7 +73,7 @@ const authenticateToken = (req, res, next) => {
 };
 //npm install @tensorflow-models/body-pix @tensorflow/tfjs
 router.put('/signup',authenticateToken ,upload.single("imgSource"),async (req, res) => {
-  const { oldUser,newUser,room } = req.body;
+  var { oldUser,newUser,room } = req.body;
   oldUser= JSON.parse(oldUser);
   newUser = JSON.parse(newUser);
   var command ={entryparams:{fieldName:"user_info",operation:"update_user_info"},
