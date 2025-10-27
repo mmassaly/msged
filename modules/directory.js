b19ce2b60c5bfddae5759a819d28b13ea0558a1d
Mamadou Massaly <massalymamadou@gmail.com>
2025-10-27 21:40:34 +0000

    GO

--- modules/directory.js
+++ modules/directory.js
@@ -600,7 +600,7 @@ router.delete('/', authenticateToken, (req, res) => {
     const removeDirectory = (dirs) => {
      
         const foundDir = recursiveFindDir(dirs,path);
-        if (foundDir !== -1) {
+        if (foundDir) {
             try
             {
                 fs.unlinkSync(path.join("./",foundDir.path));
@@ -627,7 +627,7 @@ router.delete('/', authenticateToken, (req, res) => {
 function recursiveFindDir (dir,givenPath){ 
     if(dir.path == givenPath)
         return dir;
-    var subdirfound = dir.subdirectories.find(subdir =>{console.log(subdir.path); return subdir.path == givenPath});
+    var subdirfound = dir.subdirectories.find(subdir =>{console.log(subdir.path+'='+givenPath+" is "+ subdir.path == givenPath); return subdir.path == givenPath});
     if(subdirfound !=  undefined)
     {
         return subdirfound;
