49b63543842c3a7aab9228721a9cad49f5fb859c
mmassaly <massalymamadou@gmail.com>
2026-03-09 09:58:47 +0000

    GO

--- modules/services/openAI.js
+++ modules/services/openAI.js
@@ -180,7 +180,7 @@ of the curriculum vitae content in the following text:\n${text}`
 // }).catch(error => {console.error("Error processing PDF:", error);});
 
 
-let GEMINI_API_KEY = process.env.GEMINI_API_KEY||'AIzaSyBeftC7Vt_Wn9qAhk3ZR4yqt7AILbT7mc0'; 
+let GEMINI_API_KEY = process.env.GEMINI_API_KEY; 
 const ai = new genaiPack.GoogleGenAI({apiKey: GEMINI_API_KEY});
 //ai.models.list().then(models=>console.log(models));
 
